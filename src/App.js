c7e2ae1d1b5e187c619d9681ae6517333a07f952
atomic <atomic@gmail.com>
2023-10-28 17:10:09 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/any",
+    name: "김동현",
+    birthday: "9999",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/any",
+    name: "김동현 22",
+    birthday: "9999",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/any",
+    name: "김동현 33",
+    birthday: "999",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2> Let's develop management system!</h2>
+    <div>
+      {customers.map((c) => {
+        return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />
+        );
+      })}
     </div>
   );
 }
