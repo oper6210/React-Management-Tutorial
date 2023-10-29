219d02ecd087a98f8dfede544539a6e3655fab17
atomic <atomic@gmail.com>
2023-10-29 18:39:03 +0900

    Add Get Customers Rest Api Module

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from "react";
 import "./App.css";
 import Customer from "./components/Customer";
 import Paper from "@material-ui/core/Paper";
@@ -19,66 +20,59 @@ const styles = (theme) => ({
   },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/any",
-    name: "김동현",
-    birthday: "9999",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/any",
-    name: "김동현 22",
-    birthday: "9999",
-    gender: "남자",
-    job: "대학생",
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/any",
-    name: "김동현 33",
-    birthday: "999",
-    gender: "남자",
-    job: "대학생",
-  },
-];
+class App extends Component {
+  state = {
+    customers: "",
+  };
 
-function App(props) {
-  const { classes } = props; // 클래스를 props로 전달받아 사용
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map((c) => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
+  render() {
+    const { classes } = this.props; // 클래스를 props로 전달받아 사용
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers
+              ? this.state.customers.map((c) => {
+                  return (
+                    <Customer
+                      key={c.id}
+                      id={c.id}
+                      image={c.image}
+                      name={c.name}
+                      birthday={c.birthday}
+                      gender={c.gender}
+                      job={c.job}
+                    />
+                  );
+                })
+              : ""}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5001;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "Hello!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "김동현",
+      birthday: "9999",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "김동현 22",
+      birthday: "9999",
+      gender: "남자",
+      job: "대학생",
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "김동현 33",
+      birthday: "999",
+      gender: "남자",
+      job: "대학생",
+    },
+  ]);
 });
 
 app.listen(port, () => console.log("listening on port " + port));
